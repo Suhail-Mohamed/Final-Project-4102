fec1ac5a75dd7b53d24f78ca770d63b5d65937bb
Suhail Mohamed <suhailam2000@gmail.com>
2023-03-09 15:09:57 -0500

    Project structure

---
+++ tests/test_detection.py
@@ -0,0 +1,39 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+from fer import FER
+import cv2
+
+files = ["../images/happy_lebron.jpg", "../images/another_happy_lebron.jpg", "../images/willy_wonka_smiling.jpg",
+         "../images/sparta_angry.jpg"]
+
+detector = FER()
+
+for f in files:
+    img = cv2.imread(f)
+    detector.detect_emotions(img)
+    emotion, score = detector.top_emotion(img)
+    print(emotion)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
